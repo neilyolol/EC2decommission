2018d3bf439cb4fa3d3ed0899a4585d66f5279b1
Neilyolol <yckasim00@163.com>
2015-03-17 12:12:24 +0800

    update

--- EC2decommission.py
+++ EC2decommission.py
@@ -2,8 +2,16 @@
 #Verion 0.1
 #Author: Chao
 #Created @20150317
-
+#Only support virgina and oregon
 import boto.ec2, boto.ec2.elb
 import sys, time, argparse, ConfigParser
+
+def get_region(instance_name):
+    region_head = instance_name.split('-')[0]
+    if region_head == 'ec1':
+        region = 'us-east-1'
+    elif region_head == 'ec2':
+        region = 'us-west-2'
+    return region
 
 
